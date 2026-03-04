6182239b092093449fbaff9a497c9bbca299c77f
Gokul R <gokulravivcr05@gmail.com>
2026-03-05 00:18:12 +0530

    react query setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './app/queryClient.ts';
+// import { queryClient } from 'app/queryClient';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
